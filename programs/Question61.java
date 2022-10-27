919e832b747d7800dc5cc1cfcb14c7c286cae38d
Abhishek Nagargoje <91834248+abhisheknagargoje@users.noreply.github.com>
2022-10-27 23:21:35 +0530

    Added comments to Question61.java

--- programs/Question61.java
+++ programs/Question61.java
@@ -1,14 +1,14 @@
 class Question61{  
-    public static void binarySearch(int arr[], int first, int last, int key){  
-      int mid = (first + last)/2;  
+    public static void binarySearch(int arr[], int first, int last, int key){  // function to find the key element from the array
+      int mid = (first + last)/2;  // mid index of the array
       while( first <= last ){  
          if ( arr[mid] < key ){  
-           first = mid + 1;     
+           first = mid + 1;     // searching right part of the array
          }else if ( arr[mid] == key ){  
-           System.out.println("Element is found at index: " + mid);  
+           System.out.println("Element is found at index: " + mid);  // element found on the mid index
            break;  
          }else{  
-            last = mid - 1;  
+            last = mid - 1;  // searching the left part of the array
          }  
          mid = (first + last)/2;  
       }  
@@ -17,9 +17,9 @@ class Question61{
       }  
     }  
     public static void main(String args[]){  
-           int arr[] = {10,20,30,40,50};  
-           int key = 30;  
-           int last=arr.length-1;  
+           int arr[] = {10,20,30,40,50};  // array from which the target element is being searched
+           int key = 30;   // target element to be searched
+           int last=arr.length-1;  // end index of the array
            binarySearch(arr,0,last,key);     
     }  
    }  
